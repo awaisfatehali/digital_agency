885052f55f53569296e212c2d1118eb68bc5bda9
awaisfatehali <awaisfatehali@gmail.com>
2026-05-27 22:19:40 +0500

    added a login button to the navbar for both desktop and mobile views. The button links to the Admin_Penal page for mobile and Adminlogin page for desktop.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,9 +29,11 @@ const Navbar = () => {
             Contact
           </Link>
         </div>
-        <button className="bg-blue-800 hover:bg-gray-200 hover:text-blue-800 text-lg px-6 py-3 rounded-md">
-          Request Quote
-        </button>
+        <Link to="/Adminlogin" className="text-lg">
+          <button className="bg-blue-800 hover:bg-gray-200 hover:text-blue-800 text-lg px-6 py-3 rounded-md">
+            Login
+          </button>
+        </Link>
       </div>
 
       <div className="md:hidden ml-auto mr-4">
@@ -62,9 +64,11 @@ const Navbar = () => {
           <Link to="/contact" className="text-lg">
             Contact
           </Link>
-          <button className="bg-blue-800 hover:bg-gray-200 hover:text-blue-800 text-lg px-6 py-3 rounded-md">
-            Request Quote
-          </button>
+          <Link to="/Admin_Penal" className="text-lg">
+            <button className="bg-blue-800 hover:bg-gray-200 hover:text-blue-800 text-lg px-6 py-3 rounded-md">
+              Login
+            </button>
+          </Link>
         </div>
       )}
     </div>
