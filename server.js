2d272c477fb48e3b8c667eda019458e283ca0dfb
awaisfatehali <awaisfatehali@gmail.com>
2026-05-30 01:36:12 +0500

    backend server link added

--- server.js
+++ server.js
@@ -1,2 +1,4 @@
 // export const server = "http://localhost:8000/api/v1";
-export const server = "https://agency-project-backend-git-main-awais-projects-2992b0c2.vercel.app";
+// export const server = "https://agency-project-backend-git-main-awais-projects-2992b0c2.vercel.app";
+export const server = "https://agency-project-backend.vercel.app";
+
