fa621114166a19d0b3af957075f1d0cf14cb895f
awaisfatehali <awaisfatehali@gmail.com>
2026-05-30 01:29:30 +0500

    added deplyed backend server

--- server.js
+++ server.js
@@ -1,1 +1,2 @@
-export const server = "http://localhost:8000/api/v1";
+// export const server = "http://localhost:8000/api/v1";
+export const server = "https://agency-project-backend-git-main-awais-projects-2992b0c2.vercel.app/";
