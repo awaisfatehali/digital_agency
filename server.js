da125be488bbfa29f04edcf977cffba54422ccd8
awaisfatehali <awaisfatehali@gmail.com>
2026-05-30 04:00:30 +0500

    backend server link correction

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 // export const server = "http://localhost:8000/api/v1";
 // export const server = "https://agency-project-backend-git-main-awais-projects-2992b0c2.vercel.app";
-export const server = "https://agency-project-backend.vercel.app";
+export const server = "https://agency-project-backend.vercel.app/api/v1";
 
